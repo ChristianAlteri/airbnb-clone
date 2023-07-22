89b879b4af233b3287ffa1c03d3bbc92b779f3f3
ChristianAlteri <alteri.christian@gmail.com>
2023-07-22 10:05:32 +0800

    creating modals

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/Navbar/Navbar';
+import ClientOnly from './components/ClientOnly';
+import Modal from './components/Modals/Modal';
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -22,7 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Modal isOpen={true} />
+          <Navbar />
+        </ClientOnly>
         {children}
         </body>
     </html>
